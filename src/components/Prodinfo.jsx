24de977dde8cfe95dfa06b97627a99505660097e
therajtinku <rajutinku27@gmail.com>
2024-07-26 19:21:58 +0530

    added product info

---
+++ src/components/Prodinfo.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./Navbar";
+import "./Prodinfo.css"
+const Prodinfo = ()=>{
+    return(
+        <>
+            <Navbar></Navbar>
+            <h1 className="hi">Hello</h1>
+        </>
+    )
+
+}
+export default Prodinfo;
