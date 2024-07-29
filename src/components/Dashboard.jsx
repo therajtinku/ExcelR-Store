2c0d1181d3bbb1019fead5eed6493b1f64086a64
harshwardhan111 <hardhwardhanzade5744@gmail.com>
2024-07-29 15:16:24 +0530

    update images

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -9,7 +9,7 @@ const Dashboard = () => {
             <main>
                 {/* banner section */}
                 <section className="banner">
-                    <img src="banner3.png" alt="Exclusive online discounts await!" />
+                    <img src="../src/assets/banner3.png" alt="Exclusive online discounts await!" />
                 </section>
 
                 {/* exclusive section */}
@@ -17,37 +17,37 @@ const Dashboard = () => {
                     <h2>Exclusive</h2>
                     <div className="product-carousel exclusive-carousel">
                         <div className="product">
-                            <img src="Exclusive1.jpg" alt="New Lenin Shirt" />
+                            <img src="../src/assets/Exclusive1.jpg" alt="New Lenin Shirt" />
                             <h3>New Lenin Shirt</h3>
                             <p>Rs.2999</p>
                             <button>Shop now</button>
                         </div>
                         <div className="product">
-                            <img src="Exclusive2.jpg" alt="Chikindari Kurta" />
+                            <img src="../src/assets/Exclusive2.jpg" alt="Chikindari Kurta" />
                             <h3>Chikindari Kurta</h3>
                             <p>Rs.999</p>
                             <button>Shop now</button>
                         </div>
                         <div className="product">
-                            <img src="Exclusive3.jpg" alt="Orange Blast Tee" />
+                            <img src="../src/assets/Exclusive3.jpg" alt="Orange Blast Tee" />
                             <h3>Orange Blast Tee</h3>
                             <p>Rs.799</p>
                             <button>Shop now</button>
                         </div>
                         <div className="product">
-                            <img src="Exclusive4.jpg" alt="V-neck Tee" />
+                            <img src="../src/assets/Exclusive4.jpg" alt="V-neck Tee" />
                             <h3>V-neck Tee</h3>
                             <p>Rs.2999</p>
                             <button>Shop now</button>
                         </div>
                         <div className="product">
-                            <img src="Exclusive5.jpg" alt="Hand Bag" />
+                            <img src="../src/assets/Exclusive5.jpg" alt="Hand Bag" />
                             <h3>Hand Bag</h3>
                             <p>Rs.399</p>
                             <button>Shop now</button>
                         </div>
                         <div className="product">
-                            <img src="Exclusive6.jpg" alt="Louis Devin Analog" />
+                            <img src="../src/assets/Exclusive6.jpg" alt="Louis Devin Analog" />
                             <h3>Louis Devin Analog</h3>
                             <p>Rs.2999</p>
                             <button>Shop now</button>
@@ -60,49 +60,49 @@ const Dashboard = () => {
                     <h2>FootWear</h2>
                     <div className="product-carousel footwear-carousel">
                         <div className="product">
-                            <img src="Footware1.jpg" alt="White Sneaker" />
+                            <img src="../src/assets/Footware1.jpg" alt="White Sneaker" />
                             <h3>White Sneaker</h3>
                             <p>Rs.2999 <span className="discount">50% off</span></p>
                             <button>Shop now</button>
                         </div>
                         <div className="product">
-                            <img src="Footware2.jpg" alt="Leather Shoes" />
+                            <img src="../src/assets/Footware2.jpg" alt="Leather Shoes" />
                             <h3>Leather Shoes</h3>
                             <p>Rs.2999 <span className="discount">50% off</span></p>
                             <button>Shop now</button>
                         </div>
                         <div className="product">
-                            <img src="Footware3.jpg" alt="Black Shoes" />
+                            <img src="../src/assets/Footware3.jpg" alt="Black Shoes" />
                             <h3>Black Shoes</h3>
                             <p>Rs.2999 <span className="discount">50% off</span></p>
                             <button>Shop now</button>
                         </div>
                         <div className="product">
-                            <img src="Footware4.jpg" alt="Black Leather Sneaker" />
+                            <img src="../src/assets/Footware4.jpg" alt="Black Leather Sneaker" />
                             <h3>Black Leather Sneaker</h3>
                             <p>Rs.2999 <span className="discount">50% off</span></p>
                             <button>Shop now</button>
                         </div>
                         <div className="product">
-                            <img src="Footware5.jpg" alt="Sports Shoes" />
+                            <img src="../src/assets/Footware5.jpg" alt="Sports Shoes" />
                             <h3>Sports Shoes</h3>
                             <p>Rs.2999 <span className="discount">50% off</span></p>
                             <button>Shop now</button>
                         </div>
                         <div className="product">
-                            <img src="Footware6.jpg" alt="Mid-Top Shoes" />
+                            <img src="../src/assets/Footware6.jpg" alt="Mid-Top Shoes" />
                             <h3>Mid-Top Shoes</h3>
                             <p>Rs.2999 <span className="discount">50% off</span></p>
                             <button>Shop now</button>
                         </div>
                         <div className="product">
-                            <img src="Footware7.jpg" alt="Red Tape Sneakers" />
+                            <img src="../src/assets/Footware7.jpg" alt="Red Tape Sneakers" />
                             <h3>Red Tape Sneakers</h3>
                             <p>Rs.2999 <span className="discount">50% off</span></p>
                             <button>Shop now</button>
                         </div>
                         <div className="product">
-                            <img src="Footware8.jpg" alt="Brown Leather Shoes" />
+                            <img src="../src/assets/Footware8.jpg" alt="Brown Leather Shoes" />
                             <h3>Brown Leather Shoes</h3>
                             <p>Rs.2999 <span className="discount">50% off</span></p>
                             <button>Shop now</button>
@@ -113,7 +113,7 @@ const Dashboard = () => {
                     <h2>Accessories</h2>
                     <div className="accessory-grid">
                         <div className="accessory">
-                            <img src="/Belt.jpg" alt="Belts" />
+                            <img src="../src/assets/Belt.jpg" alt="Belts" />
                             <h3>Belts</h3>
                         </div>
                         <div className="accessory">
@@ -121,7 +121,7 @@ const Dashboard = () => {
                             <h3>Rings</h3>
                         </div>
                         <div className="accessory">
-                            <img src="Jewellers.jpg" alt="Jewellers" />
+                            <img src="../src/assets/Jewellers.jpg" alt="Jewellers" />
                             <h3>Jewellers</h3>
                         </div>
                     </div>
