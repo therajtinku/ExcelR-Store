76027c7921bc85b2a59997f8453544c946f2ac26
Bhargavi <bhargavi.298.sanju@gmail.com>
2024-07-30 14:12:24 +0530

    updated

--- src/components/Wishlist.jsx
+++ src/components/Wishlist.jsx
@@ -11,6 +11,7 @@ const Wishlist = () => {
         <thead>
           <tr>
             <th>PRODUCTS</th>
+            <th></th>
             <th>PRICE</th>
             <th>STOCK STATUS</th>
             <th>ACTION</th>
@@ -21,56 +22,56 @@ const Wishlist = () => {
            {/* Add your wishlist items here */}
           <tr>
           <td>
-            <img src="/src/assets/img-1.jpg" width="120px" height="100px" alt="Error" />
-            New Lenin Shirt</td>
-            <td>$2999</td>
+            <img src="/src/assets/img-1.jpg" width="120px" height="100px" alt="Error" /></td>
+            <td>New Lenin Shirt</td>
+            <td>Rs.2999</td>
             <td style={{color:"green"}}>In Stock</td>
             <td>
-           <button style={{backgroundColor:"#925ff0", color:"whitesmoke"}}><a href="#"><i className="fa-solid fa-cart-shopping"></i>Add To Cart</a></button>
+           <button style={{backgroundColor:"#925ff0", color:"whitesmoke"}}><a href="#"><i className="fa-solid fa-cart-shopping"></i>  &nbsp;  Add To Cart</a></button>
             </td>
             <td>
-              <i className="fa-solid fa-circle-xmark"></i>
-            </td>
-          </tr>
-          {/* Add more wishlist items */}
-          <tr>
-            <td><img src="/src/assets/img-2.jpg" width="120px" height="100px" alt="Error" />
-              Chikindari Kurta</td>
-            <td>$1999</td>
-            <td style={{color:"green"}}>IN Stock</td>
-            <td>
-              <button style={{backgroundColor:"#925ff0", color:"whitesmoke"}}><a href="#"><i className="fa-solid fa-cart-shopping"></i>Add To Cart</a></button>
-            </td>
-            <td>
-              <i className="fa-solid fa-circle-xmark"></i>
+              <i className="fa-solid fa-circle-xmark" style={{fontSize:30}}></i>
             </td>
           </tr>
           {/* Add more wishlist items */}
           <tr>
+            <td><img src="/src/assets/img-2.jpg" width="120px" height="100px" alt="Error" /></td>
+              <td>Chikindari Kurta</td>
+            <td>Rs.1999</td>
+            <td style={{color:"green"}}>In Stock</td>
             <td>
-              <img src="/src/assets/img-3.jpg" width="120px" height="100px" alt="Error" />
-              White Sneakers</td>
-            <td>$1499</td>
-            <td style={{color:"green"}}>IN Stock</td>
-            <td>
-              <button style={{backgroundColor:"#925ff0", color:"whitesmoke"}}><a href="#"><i className="fa-solid fa-cart-shopping"></i>Add To Cart</a></button>
+              <button style={{backgroundColor:"#925ff0", color:"whitesmoke"}}><a href="#"><i className="fa-solid fa-cart-shopping"></i> &nbsp; Add To Cart</a></button>
             </td>
             <td>
-              <i className="fa-solid fa-circle-xmark"></i>
+              <i className="fa-solid fa-circle-xmark" style={{fontSize:30}}></i>
             </td>
           </tr>
           {/* Add more wishlist items */}
           <tr>
             <td>
-              <img src="/src/assets/img-4.jpg" width="120px" height="100px" alt="Error" />
-              White Sneakers</td>
-            <td>$1499</td>
-            <td style={{color:"green" , fontWeight:"bold"}}>IN Stock</td>
+              <img src="/src/assets/img-3.jpg" width="120px" height="100px" alt="Error" /></td>
+             <td> White Sneakers</td>
+            <td>Rs.1499</td>
+            <td style={{color:"green"}}>In Stock</td>
             <td>
-              <button style={{backgroundColor:"#925ff0", color:"whitesmoke", textalign:"center"}}><a href="#"><i className="fa-solid fa-cart-shopping"></i>Add To Cart</a></button>
+              <button style={{backgroundColor:"#925ff0", color:"whitesmoke"}}><a href="#"><i className="fa-solid fa-cart-shopping"></i> &nbsp; Add To Cart</a></button>
             </td>
             <td>
-              <i className="fa-solid fa-circle-xmark"></i>
+              <i className="fa-solid fa-circle-xmark" style={{fontSize:30}}></i>
+            </td>
+          </tr>
+          {/* Add more wishlist items */}
+          <tr>
+            <td>
+              <img src="/src/assets/img-4.jpg" width="120px" height="100px" alt="Error" /></td>
+              <td>Watch</td>
+            <td>Rs.1499</td>
+            <td style={{color:"green"}}>In Stock</td>
+            <td>
+              <button style={{backgroundColor:"#925ff0", color:"whitesmoke", textalign:"center"}}><a href="#"><i className="fa-solid fa-cart-shopping"></i> &nbsp; Add To Cart</a></button>
+            </td>
+            <td>
+              <i className="fa-solid fa-circle-xmark" style={{fontSize:30}}></i>
             </td>
           </tr>
 
