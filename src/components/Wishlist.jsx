c3b7de41418840352958c3483d7e55240afac4af
Bhargavi <bhargavi.298.sanju@gmail.com>
2024-07-30 22:46:32 +0530

    updated

--- src/components/Wishlist.jsx
+++ src/components/Wishlist.jsx
@@ -1,86 +1,75 @@
-import Navbar from './Navbar';
-import './Wishlist.css';
+import React, { useState } from "react";
+import "./Wishlist.css";
+import Navbar from "./Navbar";
 
 const Wishlist = () => {
-  return (
-    <>
-    <Navbar></Navbar>
-    <h1>WishList</h1>
-    <div>
-      <table>
-        <thead>
-          <tr>
-            <th>PRODUCTS</th>
-            <th></th>
-            <th>PRICE</th>
-            <th>STOCK STATUS</th>
-            <th>ACTION</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-           {/* Add your wishlist items here */}
-          <tr>
-          <td>
-            <img src="/src/assets/img-1.jpg" width="120px" height="100px" alt="Error" /></td>
-            <td>New Lenin Shirt</td>
-            <td>Rs.2999</td>
-            <td style={{color:"green"}}>In Stock</td>
-            <td>
-           <button style={{backgroundColor:"#925ff0", color:"whitesmoke"}}><a href="#"><i className="fa-solid fa-cart-shopping"></i>  &nbsp;  Add To Cart</a></button>
-            </td>
-            <td>
-              <i className="fa-solid fa-circle-xmark" style={{fontSize:30}}></i>
-            </td>
-          </tr>
-          {/* Add more wishlist items */}
-          <tr>
-            <td><img src="/src/assets/img-2.jpg" width="120px" height="100px" alt="Error" /></td>
-              <td>Chikindari Kurta</td>
-            <td>Rs.1999</td>
-            <td style={{color:"green"}}>In Stock</td>
-            <td>
-              <button style={{backgroundColor:"#925ff0", color:"whitesmoke"}}><a href="#"><i className="fa-solid fa-cart-shopping"></i> &nbsp; Add To Cart</a></button>
-            </td>
-            <td>
-              <i className="fa-solid fa-circle-xmark" style={{fontSize:30}}></i>
-            </td>
-          </tr>
-          {/* Add more wishlist items */}
-          <tr>
-            <td>
-              <img src="/src/assets/img-3.jpg" width="120px" height="100px" alt="Error" /></td>
-             <td> White Sneakers</td>
-            <td>Rs.1499</td>
-            <td style={{color:"green"}}>In Stock</td>
-            <td>
-              <button style={{backgroundColor:"#925ff0", color:"whitesmoke"}}><a href="#"><i className="fa-solid fa-cart-shopping"></i> &nbsp; Add To Cart</a></button>
-            </td>
-            <td>
-              <i className="fa-solid fa-circle-xmark" style={{fontSize:30}}></i>
-            </td>
-          </tr>
-          {/* Add more wishlist items */}
-          <tr>
-            <td>
-              <img src="/src/assets/img-4.jpg" width="120px" height="100px" alt="Error" /></td>
-              <td>Watch</td>
-            <td>Rs.1499</td>
-            <td style={{color:"green"}}>In Stock</td>
-            <td>
-              <button style={{backgroundColor:"#925ff0", color:"whitesmoke", textalign:"center"}}><a href="#"><i className="fa-solid fa-cart-shopping"></i> &nbsp; Add To Cart</a></button>
-            </td>
-            <td>
-              <i className="fa-solid fa-circle-xmark" style={{fontSize:30}}></i>
-            </td>
-          </tr>
+  const [wishlist, setWishlist] = useState([
+    {
+      id: 1,
+      name: "New Lenin Shirt",
+      image: "/src/assets/img-1.jpg",
+      price: 2999,
+      stock: "In Stock",
+    },
+    {
+      id: 2,
+      name: "Chikindari Kurta",
+      image: "/src/assets/img-2.jpg",
+      price: 1999,
+      stock: "In Stock",
+    },
+    {
+      id: 3,
+      name: "White Sneakers",
+      image: "/src/assets/img-3.jpg",
+      price: 1499,
+      stock: "In Stock",
+    },
+    {
+      id: 4,
+      name: "Watch",
+      image: "/src/assets/img-4.jpg",
+      price: 1499,
+      stock: "In Stock",
+    },
+  ]);
 
-        </tbody>
-      </table>
+  const handleRemoveFromWishlist = (itemId) => {
+    setWishlist(wishlist.filter((item) => item.id !== itemId));
+  };
+
+  return (
+    <div className="wishlist-container">
+      <h1>WishList</h1>
+      <div className="wishlist-table">
+        <div className="table-header">
+          <div>PRODUCTS</div>
+          <div></div>
+          <div>PRICE</div>
+          <div>STOCK STATUS</div>
+          <div>ACTION</div>
+          <div></div>
+        </div>
+        {wishlist.map((item) => (
+          <div key={item.id} className="table-row">
+            <div className="product-image">
+              <img src={item.image} alt={item.name} />
+            </div>
+            <div>{item.name}</div>
+            <div>Rs.{item.price}</div>
+            <div style={{color:"green"}}>{item.stock}</div>
+            <div className="cart-button">
+            <button style={{backgroundColor:"#925ff0", color:"whitesmoke", textalign:"center"}}><a href="#"><i className="fa-solid fa-cart-shopping"></i>  &nbsp;  Add To Cart</a></button>
+          </div>
+            <div className="remove">
+               <i className="fa-solid fa-circle-xmark"
+                onClick={() => handleRemoveFromWishlist(item.id)}>
+                </i>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
-    
-      
-    </>
   );
 };
 
