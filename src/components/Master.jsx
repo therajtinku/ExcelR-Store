810b8d025d931fdb53303265f32ff2f3c32b42d8
therajtinku <rajutinku27@gmail.com>
2024-07-30 22:21:42 +0530

    updated

--- src/components/Master.jsx
+++ src/components/Master.jsx
@@ -2,11 +2,13 @@ import {BrowserRouter, Route, Routes,Link} from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import Dashboard from "./Dashboard";
+import Billing from "./Billing"
 const Master = ()=>{
     return(
         <>
         <Navbar></Navbar>
         <Dashboard></Dashboard>
+        <Billing></Billing>
 
             {/* <BrowserRouter>
             <Link to="/comp3">comp3</Link>
